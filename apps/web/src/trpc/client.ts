38892d6a8aafdd8d24c6fb9dbdc1f3ef39e01665
Friedrich482 <friedricht109@gmail.com>
2025-08-28 17:17:06 +0200

    refactor: individual posts - updated trp client and server to v11.5.0 -  removed ts-expect-error from the trpc client declaration

--- apps/web/src/trpc/client.ts
+++ apps/web/src/trpc/client.ts
@@ -4,7 +4,6 @@ import superjson from "superjson";
 
 import { API_URL } from "@/constants";
 
-// @ts-expect-error: createTRPCClient type mismatch due to trpc internal errors
 const trpc = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
