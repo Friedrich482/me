d9866e09fc186d44989d2849b594b72a77d1b431
Friedrich482 <friedricht109@gmail.com>
2025-09-20 16:59:20 +0200

    fix: theme - fixed the opacity of the blog post list in light mode

--- apps/admin/src/components/posts/PostsList.tsx
+++ apps/admin/src/components/posts/PostsList.tsx
@@ -16,17 +16,17 @@ const PostsList = ({ status }: { status: Status }) => {
   return posts.length === 0 ? (
     <p className="text-xl">No posts yet.</p>
   ) : (
-    <ul className="text-foreground/60 w-full space-y-8 text-lg">
+    <ul className="text-foreground/75 dark:text-foreground/60 space-y-8 text-lg">
       {posts.map((post) => (
         <li
           key={post.id}
-          className="hover:text-primary/75 w-full cursor-pointer text-start"
+          className="hover:text-primary/95 dark:hover:text-primary/75 w-full cursor-pointer text-start"
         >
           <Link to={`/posts/${post.slug}`} className="flex gap-2">
             <CircleSmall className="shrink-0" />
             <div className="space-x-4">
               <span>{post.title}</span>
-              <span className="opacity-50">
+              <span className="opacity-70 dark:opacity-50">
                 {post.publishDate
                   ? format(post.publishDate, "MMM dd, yyyy")
                   : post.status === "draft" && status !== "draft"
